20c1286fd14f5218acf5e187c22eb826e636d8ae
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-03-19 19:29:40 -0300

    Add theme

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+
+import theme from './src/global/styles/theme';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+      <StatusBar style="light" />
+    </ThemeProvider>
+  )
+}
+
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import {Container} from './styles'
+
+export function Home(){
+    return(
+        <Container>
+            <Text>Homepage</Text>
+        </Container>
+    )
+}
